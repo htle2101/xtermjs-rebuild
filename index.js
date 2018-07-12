b593ce60bf597e0df9d8a0e802d216ca44599f8f
liht <liht@Ctrip.com>
2018-07-12 11:27:35 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+var os = require('os');
+var pty = require('node-pty');
+var shell = os.platform() === 'win32' ? 'powershell.exe' : '/bin/zsh';
+
 var Terminal = require('./lib/xterm');
+
+var ptyProcess = pty.spawn(shell, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.env.HOME,
+  env: process.env
+});
 
 var term = new Terminal();
 term._core.browser.isMac = true;
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-console.log(term);
+
+ptyProcess.on('data', function(data) {
+  term.write(data)
+});
+
+ptyProcess.write('ls -l\n\r');
+
+setTimeout(function(){
+    console.log(term._core.buffer.lines._array[0])
+    console.log(term._core.buffer.lines._array[1])
+    console.log(term._core.buffer.lines._array[2])
+    console.log(term._core.buffer.lines._array[3])
+}, 5000)
+
+
